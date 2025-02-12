3adc82aba211de0306be523a88312eb2cbfb7285
Ernesto Sos <ernestososruiz@gmail.com>
2025-02-12 19:40:13 +0100

    meto el import os

--- src/data/db.py
+++ src/data/db.py
@@ -1,3 +1,4 @@
+import os
 from sqlmodel import create_engine, SQLModel, Session
 from src.models.coche import Coche
 
@@ -8,7 +9,7 @@ db_port: int = 3306  #SE PONE EL PUERTO DEFECTO DE DOCKER
 db_name: str = "cochesdb"  
 
 DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_server}:{db_port}/{db_name}"
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(os.getenv("db_url",DATABASE_URL), echo=True)
 
 def get_session():
     with Session(engine) as session:
